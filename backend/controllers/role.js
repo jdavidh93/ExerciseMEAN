5340006f82c84bc1c731d538a12195543881a444
jdavidh93 <jossedavidhurtado@hotmail.com>
2021-11-09 15:53:37 -0500

    Se continua proyecto(Codigo para actualizar y eliminar Role)

--- backend/controllers/role.js
+++ backend/controllers/role.js
@@ -19,12 +19,52 @@ const registerRole = async (req, res) => {
 };
 
 //Es para hacer las consultas.
-const listRole = async (req, res) =>{
+const listRole = async (req, res) => {
   const roleSchema = await role.find();
-  if(!roleSchema || roleSchema.length ==0) return res.status(400).send("Empty role list");
-  return res.status(200).send({roleSchema});
+  if (!roleSchema || roleSchema.length == 0)
+    return res.status(400).send("Empty role list");
+  return res.status(200).send({ roleSchema });
 
-  //return !roleSchema || roleSchema.length==0 ? res.status(400).send("Empty role list") : res.status(200).send({roleSchema}); //Operador contrario para consultar 
-}
+  //return !roleSchema || roleSchema.length==0 ? res.status(400).send("Empty role list") : res.status(200).send({roleSchema}); //Operador contrario para consultar
+};
 
-export default { registerRole, listRole };
+//Para realizar la actualizacion de un rol.
+const updateRole = async (req, res) => {
+  if (!req.body.name || !req.body.description)
+    return res.status(400).send("Imcomplete data");
+
+  const existinRole = await role.findOne({
+    name: req.body.name,
+    description: req.body.description,
+  });
+  if (existinRole) return res.status(400).send("The rol already exist");
+
+  const roleUpdate = await role.findByIdAndUpdate(req.body._id, {
+    name: req.body.name,
+    description: req.body.description,
+  });
+
+  return !roleUpdate
+    ? res.status(400).send("Error editing role")
+    : res.status(200).send({ roleUpdate });
+};
+
+//Para eliminar los roles.
+const deleteRole = async (req, res) => {
+  const roleDelete = await role.findByIdAndDelete({ _id: req.params["_id"] });
+  return !roleDelete
+    ? res.status(400).send("Role no found")
+    : res.status(200).send("Role deleted");
+};
+
+//Se realiza interno.Cuando se hace login para identificar el rol.
+//jwt para el login{dsdpwnoiewfnoen4nr0f0ene}
+const findRole = async (req, res) => {
+  const roleId = await role.findById({ _id: req.params["_id"] });
+  //if (!roleId) return res.status
+  return !roleId
+    ? res.status(400).send("No search results")
+    : res.status(200).send({ roleId });
+};
+
+export default { registerRole, listRole, findRole, updateRole, deleteRole };
--- backend/routes/role.js
+++ backend/routes/role.js
@@ -5,5 +5,8 @@ const router  = express.Router()
 
 router.post("/registerRole", role.registerRole);
 router.get("/listRole", role.listRole);
+//router.get("/findRole/:_id", role.findRole);
+router.put("/updateRole", role.updateRole);
+router.delete("/deleteRole/:_id", role.deleteRole);
 
 export default router   
